0dd31cbf8a934a3af3cc3a93b9e8af96f1e7835b
JayashTripathy <jayashtripathy371@gmail.com>
2023-11-23 12:02:33 +0530

    added a browser check in useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 
 export const useLocalStorage = <T>(key: string, defaultValue: T) => {
+  const isBrowser = typeof window !== "undefined";
   const [value, setValue] = useState<T | null>(() => {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : defaultValue;
   });
-  
+
   useEffect(() => {
-    if (value) {
+    if (value && isBrowser) {
       localStorage.setItem(key, JSON.stringify(value));
     }
-  }, [key, value]);
+  }, [key, value, isBrowser]);
 
   return [value, setValue] as const;
 };
